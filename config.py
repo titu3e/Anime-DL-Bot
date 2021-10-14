604479f1a14c7f1f912e079b9f0dfa94050fff01
titu3e <75441242+titu3e@users.noreply.github.com>
2021-10-14 15:36:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@
 
 """ANIME DL TELEGRAM BOT CREDENTIALS"""
 
-api_id = ""
-api_hash = ""
-bot_token = ""
+api_id = "8626831"
+api_hash = "db23330a6edf4a517ee186b35cedec71"
+bot_token = "2056757080:AAG92-qzuYiqcT1vTQY5FLFh0amnDly5bCY"
